49985b65006e0c68424562bb7fef6f4d74c202b6
Soumendra Kumar Sahoo <soumend1@in.ibm.com>
2020-06-15 14:42:18 +0530

    intermediate commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # corpus
 Different types of Odia language data with their sources
+
+#### Running command
+ python ../scripts/wikipedia/wikipedia_article_extractor.py 
+
+
+# Acknowledgements
+- [Gaurav Arora](https://github.com/goru001/nlp-for-odia) for Wikipedia scraping algorithm
+- [Adnan Siddiqi](https://towardsdatascience.com/5-strategies-to-write-unblock-able-web-scrapers-in-python-5e40c147bdaf) for headers of data scraping
+
--- scripts/wikipedia/wikipedia_article_extractor.py
+++ scripts/wikipedia/wikipedia_article_extractor.py
@@ -6,17 +6,19 @@ Start date: 14 May 2020
 Inspired from: https://github.com/goru001/nlp-for-odia
 Reference: https://towardsdatascience.com/5-strategies-to-write-unblock-able-web-scrapers-in-python-5e40c147bdaf
 """
+import asyncio
 import random
-from time import sleep
-from typing import List
+from typing import Dict
 
+import aiofiles as aiofiles
+import aiohttp
 import os
 import re
 import requests
 from bs4 import BeautifulSoup
 from requests.adapters import HTTPAdapter
-from urllib3 import Retry
 from tqdm import tqdm
+from urllib3 import Retry
 
 SESSION = requests.session()
 RETRIES = Retry(total=5, backoff_factor=0.2, status_forcelist=[500, 501, 502, 503, 504])
@@ -34,11 +36,11 @@ HEADERS = {
 }
 
 
-def fetch_article_links() -> List[str]:
+def fetch_article_links() -> Dict[str, str]:
     """
     Fetches all the 52 alphabets links from the Wikipedia
-    :return: List of links
-    :rtype: List[str]
+    :return: Dictionary of links with its title as values
+    :rtype: Dict[str, str]
     """
     with SESSION:
         response = SESSION.get("https://or.wikipedia.org", headers=HEADERS)
@@ -49,7 +51,7 @@ def fetch_article_links() -> List[str]:
     )
     anchors = tab.find_all("a")
     home_url = "https://or.wikipedia.org"
-    links = [home_url + anchor["href"] for anchor in anchors][:2]
+    links = [home_url + anchor["href"] for anchor in anchors][:1]
     prev_len = 0
     with SESSION:
         for link in tqdm(links):
@@ -59,43 +61,61 @@ def fetch_article_links() -> List[str]:
                 div = soup.find("div", {"class": "mw-allpages-body"})
                 if div:
                     anchors = div.find_all("a")
-                    all_links = [home_url + anchor["href"] for anchor in anchors]
-                if prev_len == len(set(all_links)):
+                    all_links = {
+                        anchor.text: home_url + anchor["href"] for anchor in anchors
+                    }
+                if prev_len == len(all_links):
                     break
                 nav_div = soup.find("div", {"class": "mw-allpages-nav"})
                 if nav_div and len(nav_div.find_all("a")) == 2:
                     link = home_url + nav_div.find_all("a")[1]["href"]
-                prev_len = len(set(all_links))
+                prev_len = len(all_links)
     return all_links
 
 
-def write_link_text(all_links_: List[str]) -> None:
+async def write_link_text(url, filename, session) -> None:
     """
     Extracts data from the links and write into the files
-    :param all_links_:
-    :type all_links_:
+    :param url:
+    :type url:
+    :param filename:
+    :type filename:
+    :param session:
+    :type session:
     :return:
     :rtype:
     """
-    filename = "Not created yet"
-    try:
-        for counter, url in tqdm(enumerate(all_links_)):
-            delays = [2, 5, 1, 7, 4, 9]
-            sleep(random.choice(delays))
-            with SESSION:
-                link_response = SESSION.get(url, headers=HEADERS)
-            link_soup = BeautifulSoup(link_response.text, PARSER)
-            paras = link_soup.find_all("p")
-            article = "\n".join([para.text for para in paras])
-            article = process_text(article)
-            filename = OUTPUT_PATH + str(counter) + ".txt"
-            with open(filename, "w") as output_file:
-                output_file.write(article)
-    except BaseException as error:
-        print(f"unable to write file: {filename} due to: {error}")
+    async with session.get(url, headers=HEADERS) as link_response:
+        html = await link_response.read()
+        link_soup = BeautifulSoup(html, PARSER)
+        paras = link_soup.find_all("p")
+        article = "\n".join([para.text for para in paras])
+        article = await process_text(article)
+        async with aiofiles.open(filename, "w+") as output_file:
+            print(f"Writing into file: {filename}")
+            await output_file.write(article)
+        return await link_response.release()
 
 
-def process_text(article_text: str) -> str:
+async def main(title, url):
+    """
+    Main processor
+    :param title:
+    :type title:
+    :param url:
+    :type url:
+    :return:
+    :rtype:
+    """
+    async with aiohttp.ClientSession() as session:
+        filename = OUTPUT_PATH + title + ".txt"
+        print(f"Fetching the article: {title} with URL: {url}")
+        delays = [2, 5, 1, 7, 4, 9]
+        await asyncio.sleep(random.choice(delays))
+        await write_link_text(url, filename, session)
+
+
+async def process_text(article_text: str) -> str:
     """
     Process the text assigned to it
     :param article_text:
@@ -115,4 +135,6 @@ def process_text(article_text: str) -> str:
 
 
 if __name__ == "__main__":
-    write_link_text(fetch_article_links())
+    all_links = fetch_article_links()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.gather(*(main(title, url) for title, url in all_links)))
